040b4140fb83d91797f6c931567ea8600b5e90ef
Daniel Iser <danieliser@wizardinternetsolutions.com>
2019-01-20 00:17:21 -0500

    Updated build routines & packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,101 @@
-var gulp = require('gulp'),
-    runSequence = require('run-sequence').use(gulp),
-    $fn = require('gulp-load-plugins')({ camelize: true }),
+var fs = require('fs'),
+    path = require('path'),
+    merge = require('merge-stream'),
+    gulp = require('gulp'),
+    $fn = require('gulp-load-plugins')({camelize: true}),
     plumberErrorHandler = {
         errorHandler: $fn.notify.onError({
             title: 'Gulp',
             message: 'Error: <%= error.message %>'
         })
     },
-    pkg = require('./package.json');
+    cleanCSS = require('gulp-clean-css'),
+    pkg = require('./package.json'),
 
-//region JavaScript
-gulp.task('js:admin', function() {
-    return gulp.src(['assets/scripts/src/admin/vendor/*.js', 'assets/scripts/src/admin/plugins/**/*.js', 'assets/scripts/src/admin/general.js'])
+    // customize these
+    script_src_path = 'assets/js/src',
+    site_script_src_path = path.join(script_src_path, 'site'),
+    admin_script_src_path = path.join(script_src_path, 'admin'),
+    sass_src_path = 'assets/sass',
+    script_output_path = 'assets/js',
+    css_output_path = 'assets/css';
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function (file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
+function langpack() {
+    return gulp.src(['**/*.php', '!build/**/*.*'], {allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.jshint())
-        .pipe($fn.jshint.reporter('default'))
-        .pipe($fn.order([
-            "vendor/**/*.js",
-            "plugins/**/*.js",
-            'general.js'
-        ], { base: 'assets/scripts/src/admin/' }))
-        .pipe($fn.concat('admin.js'))
-        .pipe(gulp.dest('assets/scripts'))
-        .pipe($fn.uglify())
-        .pipe($fn.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/scripts'))
-        .pipe($fn.notify({
-            message: 'Admin JS task complete',
-            onLast: true
+        .pipe($fn.sort())
+        .pipe($fn.wpPot({
+            domain: pkg.name,
+            package: pkg.description,
+            bugReport: 'https://wppopupmaker.com/support/',
+            team: 'WP Popup Maker <support@wppopupmaker.com>'
         }))
-        .pipe($fn.livereload());
-});
+        .pipe(gulp.dest('languages/' + pkg.name + '.pot'));
+}
 
-gulp.task('js:site', function() {
-    return gulp.src(['assets/scripts/src/site/plugins/**/*.js', 'assets/scripts/src/site/general.js'])
+langpack.description = "Generate language files";
+
+function clean_langpack() {
+    return gulp.src(['languages/*.pot'], {read: false, allowEmpty: true})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+}
+
+clean_langpack.description = "Purge language files";
+
+function js_admin() {
+    var folders = getFolders(admin_script_src_path),
+        // process each sub-folder
+        tasks = folders.map(function (folder) {
+            return gulp.src(path.join(admin_script_src_path, folder, '/**/*.js'), {allowEmpty: true})
+                .pipe($fn.plumber(plumberErrorHandler))
+                .pipe($fn.jshint())
+                .pipe($fn.jshint.reporter('default'))
+                .pipe($fn.order([
+                    "vendor/**/*.js",
+                    "plugins/**/*.js",
+                    'general.js'
+                ], {base: path.join(admin_script_src_path, folder)}))
+                // concat into foldername.js
+                .pipe($fn.concat('admin-' + folder + '.js'))
+                .pipe(gulp.dest(script_output_path))
+                .pipe($fn.uglify())
+                .pipe($fn.rename({extname: '.min.js'}))
+                .pipe(gulp.dest(script_output_path));
+        }),
+        // process all remaining files in admin_script_src_path root into main.js and main.min.js files
+        root = gulp.src(path.join(admin_script_src_path, '/*.js'), {allowEmpty: true})
+            .pipe($fn.plumber(plumberErrorHandler))
+            .pipe($fn.jshint())
+            .pipe($fn.jshint.reporter('default'))
+            .pipe($fn.rename({prefix: 'admin-'}))
+            .pipe(gulp.dest(script_output_path))
+            .pipe($fn.uglify())
+            .pipe($fn.rename({extname: '.min.js'}))
+            .pipe(gulp.dest(script_output_path));
+
+    return merge(tasks, root);
+}
+
+js_admin.description = "Build admin Javascript assets.";
+
+function clean_js_admin() {
+    return gulp.src(path.join(script_output_path, '/admin*.js'), {read: false, allowEmpty: true})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+}
+
+clean_js_admin.description = "Purge admin Javascript build files.";
+
+function js_site() {
+    return gulp.src([path.join(site_script_src_path, '/**/*.js')], {allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
         .pipe($fn.jshint())
         .pipe($fn.jshint.reporter('default'))
@@ -42,61 +104,58 @@ gulp.task('js:site', function() {
             "plugins/pum.js",
             "plugins/**/*.js",
             'general.js'
-        ], { base: 'assets/scripts/src/site/' }))
+        ], {base: site_script_src_path}))
         .pipe($fn.concat('site.js'))
-        .pipe(gulp.dest('assets/scripts'))
+        .pipe(gulp.dest(script_output_path))
         .pipe($fn.uglify())
         .pipe($fn.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/scripts'))
-        .pipe($fn.notify({
-            message: 'Site JS task complete',
-            onLast: true
-        }))
-        .pipe($fn.livereload());
-});
+        .pipe(gulp.dest(script_output_path));
+}
 
-gulp.task('js:other', function() {
-    return gulp.src('assets/scripts/src/*.js')
+js_site.description = "Build site Javascript assets.";
+
+function clean_js_site() {
+    return gulp.src(path.join(script_output_path, '/site*.js'), {read: false, allowEmpty: true})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+}
+
+clean_js_site.description = "Purge site Javascript build files.";
+
+function js_other() {
+    return gulp.src(path.join(script_src_path, '*.js'), {allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
         .pipe($fn.jshint())
         .pipe($fn.jshint.reporter('default'))
-        .pipe(gulp.dest('assets/scripts'))
+        .pipe(gulp.dest(script_output_path))
         .pipe($fn.uglify())
         .pipe($fn.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/scripts'))
-        .pipe($fn.notify({
-            message: 'Other JS task complete',
-            onLast: true
-        }))
-        .pipe($fn.livereload());
-});
+        .pipe(gulp.dest(script_output_path));
+}
 
-gulp.task('js', ['js:admin', 'js:site', 'js:other']);
-//endregion JavaScript
+js_other.description = "Build 3rd party Javascript assets.";
 
-//region Language Files
-gulp.task('langpack', function () {
-    return gulp.src(['**/*.php', '!build/**/*.*'])
+function clean_js_other() {
+    return gulp.src([path.join(script_output_path, '/*.js'), '!' + path.join(script_output_path, 'site*.js'), '!' + path.join(script_output_path, 'admin*.js')], {
+        read: false,
+        allowEmpty: true
+    })
         .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.sort())
-        .pipe($fn.wpPot( {
-            domain: pkg.name,
-            bugReport: 'danieliser@wizardinternetsolutions.com',
-            team: 'Daniel Iser <danieliser@wizardinternetsolutions.com>'
-        } ))
+        .pipe($fn.clean());
+}
 
-        .pipe(gulp.dest('languages'))
-        .pipe($fn.notify({
-            message: 'Language files task complete',
-            onLast: true
-        }));
-});
-//endregion Language Files
+clean_js_other.description = "Purge 3rd party Javascript build files.";
 
-//region SASS & CSS
-gulp.task('css', function() {
-    return gulp.src('assets/styles/sass/*.scss')
+function css() {
+    return gulp.src(path.join(sass_src_path, '/*.s+(a|c)ss'))
         .pipe($fn.plumber(plumberErrorHandler))
+        // .pipe($fn.sassLint({
+        //     ignore: {
+        //         'no-color-literals': 0
+        //     }
+        // }))
+        // .pipe($fn.sassLint.format())
+        // .pipe($fn.sassLint.failOnError())
         .pipe($fn.sourcemaps.init())
         .pipe($fn.sass({
             errLogToConsole: true,
@@ -110,127 +169,142 @@ gulp.task('css', function() {
         .pipe($fn.autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe($fn.sourcemaps.write('.'))
         .pipe($fn.plumber.stop())
-        .pipe(gulp.dest('assets/styles'))
+        .pipe(gulp.dest(css_output_path))
         .pipe($fn.filter('**/*.css')) // Filtering stream to only css files
         .pipe($fn.combineMq()) // Combines Media Queries
-        .pipe($fn.livereload())
-        .pipe($fn.rename({ suffix: '.min' }))
-        .pipe($fn.csso({
-            //sourceMap: true,
-        }))
-        .pipe(gulp.dest('assets/styles'))
-        .pipe($fn.livereload())
-        .pipe($fn.notify({
-            message: 'Styles task complete',
-            onLast: true
-        }))
-        .pipe(gulp.dest('assets/styles'));
-});
-//endregion SASS & CSS
+        .pipe($fn.rename({suffix: '.min'}))
+        .pipe(cleanCSS({
+            level: {
+                1: {
+                    specialComments: false
+                },
+                2: {
+                    all: true
+                }
+            }
 
-//region Cleaners
-gulp.task('clean-js:site', function () {
-    return gulp.src('assets/scripts/site*.js', {read: false})
+        }))
+        .pipe(gulp.dest(css_output_path));
+}
+
+css.description = "Build css assets from sass.";
+
+function clean_css() {
+    return gulp.src([path.join(css_output_path, '/*.css'), path.join(css_output_path, '*.css.map')], {
+        read: false,
+        allowEmpty: true
+    })
         .pipe($fn.plumber(plumberErrorHandler))
         .pipe($fn.clean());
-});
-gulp.task('clean-js:admin', function () {
-    return gulp.src('assets/scripts/admin*.js', {read: false})
+}
+
+clean_css.description = "Purge css build files.";
+
+function clean_build() {
+    return gulp.src('./build/**/*.*', {read: false, allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
         .pipe($fn.clean());
-});
-gulp.task('clean-js:other', function () {
-    return gulp.src(['assets/scripts/*.js', '!assets/scripts/site*.js', '!assets/scripts/admin*.js'], {read: false})
-        .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.clean());
-});
-gulp.task('clean-css', function () {
-    return gulp.src(['assets/styles/*.css', 'assets/styles/*.css.map'], {read: false})
-        .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.clean());
-});
-gulp.task('clean-build', function () {
-    return gulp.src('build/*', {read: false})
-        .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.clean());
-});
-gulp.task('clean-package', function () {
-    return gulp.src('release/'+pkg.name+'_v'+pkg.version+'.zip', {read: false})
+}
+
+clean_build.description = "Purge compiled plugin build files & folder.";
+
+function clean_package() {
+    return gulp.src('release/' + pkg.name + '_v' + pkg.version + '.zip', {read: false, allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
         .pipe($fn.clean({force: true}));
-});
+}
 
-// Cleaning Routines
-gulp.task('clean-js', function (done) {
-    runSequence(
-        ['clean-js:site', 'clean-js:admin', 'clean-js:other'],
-        done
-    );
-});
-gulp.task('clean-all', function (done) {
-    runSequence(
-        ['clean-js', 'clean-css'],
-        ['clean-build', 'clean-package'],
-        done
-    );
-});
-//endregion Cleaners
+clean_package.description = "Purge packaged release zip file.";
 
-//region Watch & Build
-gulp.task('watch', function () {
-    $fn.livereload.listen();
-    gulp.watch('assets/styles/sass/**/*.scss', ['css']);
-    gulp.watch('assets/scripts/src/admin/**/*.js', ['js:admin']);
-    gulp.watch('assets/scripts/src/site/**/*.js', ['js:site']);
-    gulp.watch(['assets/scripts/src/**/*.js', '!assets/scripts/src/site/**/*.js', '!assets/scripts/src/admin/**/*.js'], ['js:other']);
-    gulp.watch('**/*.php', ['langpack']);
-});
-
-// Cleans & Rebuilds Assets Prior to Builds
-gulp.task('prebuild', function (done) {
-    runSequence(
-        'clean-all',
-        ['css', 'js', 'langpack'],
-        done
-    );
-});
-
-// Copies a clean set of build files into the build folder
-gulp.task('build', ['prebuild'], function () {
-    return gulp.src(['./**/*.*', '!./build/**', '!./release/**', '!./node_modules/**', '!./gulpfile.js', '!./package.json', '!./assets/scripts/src/**'])
+function build() {
+    return gulp.src(['./**/*.*', '!./build/**', '!./release/**', '!./node_modules/**', '!./gulpfile.js', '!./package.json', '!./assets/js/src/**'], {allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
-        .pipe(gulp.dest('build/'+pkg.name));
-});
+        .pipe(gulp.dest('build/' + pkg.name));
+}
 
-// Generates a release package with the current version from package.json
-gulp.task('package', ['clean-package'], function () {
-    return gulp.src('build/**/*.*')
+build.description = "Copies a clean set of build files into the build folder.";
+
+function package() {
+    return gulp.src('build/**', {allowEmpty: true})
         .pipe($fn.plumber(plumberErrorHandler))
-        .pipe($fn.zip(pkg.name+'_v'+pkg.version+'.zip'))
+        .pipe($fn.zip(pkg.name + '_v' + pkg.version + '.zip'))
         .pipe(gulp.dest('release'));
-});
+}
 
-// Runs all build routines and generates a release.
-gulp.task('release', function (done) {
-    runSequence(
-        'build',
-        'package',
-        done
-    );
-});
+package.description = "Generates a release package with the current version from package.json";
 
-// Runs a releaes and cleans up afterwards.
-gulp.task('release:clean', ['release'], function (done) {
-    runSequence(
-        'clean-build',
-        done
-    );
-});
-//endregion Watch & Build
+function sass_watcher() {
+    $fn.saneWatch(path.join(sass_src_path, '/**/*.s+(a|c)ss'), {debounce: 300}, gulp.parallel('css'));
+}
 
-gulp.task('default', function (done) {
-    runSequence(
-        'prebuild',
-        'watch'
-    );
-});
+sass_watcher.description = "Starts a scss/sass file watcher.";
+
+function js_admin_watcher() {
+    $fn.saneWatch(path.join(admin_script_src_path, '/**/*.js'), {debounce: 300}, gulp.parallel('js_admin'));
+}
+
+js_admin_watcher.description = "Starts admin Javascript file watcher.";
+
+function js_site_watcher() {
+    $fn.saneWatch(path.join(site_script_src_path, '/**/*.js'), {debounce: 300}, gulp.parallel('js_site'));
+}
+
+js_site_watcher.description = "Starts site Javascript file watcher.";
+
+function js_other_watcher() {
+    $fn.saneWatch(path.join(script_src_path, '*.js'), {debounce: 300}, gulp.parallel('js_other'));
+}
+
+js_other_watcher.description = "Starts 3rd party Javascript file watcher.";
+
+function langpack_watcher() {
+    $fn.saneWatch('**/*.php', {debounce: 300}, gulp.parallel('langpack'));
+}
+
+langpack_watcher.description = "Starts langpack php file watcher.";
+
+gulp.task(langpack);
+gulp.task(js_admin);
+gulp.task(js_site);
+gulp.task(js_other);
+gulp.task(css);
+gulp.task(clean_js_site);
+gulp.task(clean_js_admin);
+gulp.task(clean_js_other);
+gulp.task(clean_css);
+gulp.task(clean_langpack);
+gulp.task(clean_build);
+gulp.task(clean_package);
+gulp.task(sass_watcher);
+gulp.task(js_admin_watcher);
+gulp.task(js_site_watcher);
+gulp.task(js_other_watcher);
+gulp.task(langpack_watcher);
+gulp.task('js', gulp.parallel(['js_admin', 'js_site', 'js_other']));
+gulp.task('clean_js', gulp.parallel(['clean_js_site', 'clean_js_admin', 'clean_js_other']));
+gulp.task('clean_all', gulp.parallel(['clean_js', 'clean_css', 'clean_langpack', 'clean_build', 'clean_package']));
+gulp.task('prebuild', gulp.series('clean_all', gulp.parallel('css', 'js', 'langpack')));
+gulp.task('build', gulp.series('prebuild', build));
+gulp.task('package', gulp.series('clean_package', package));
+gulp.task('release', gulp.series('build', 'package', 'clean_build'));
+gulp.task('js_watcher', gulp.parallel(['js_admin_watcher', 'js_site_watcher', 'js_other_watcher']));
+gulp.task('watch', gulp.parallel(['sass_watcher', 'js_watcher', 'langpack_watcher']));
+gulp.task('default', gulp.series('prebuild', 'watch'));
+
+var _default = gulp.task('default'),
+    js = gulp.task('js'),
+    clean_js = gulp.task('clean_js'),
+    clean_all = gulp.task('clean_all'),
+    prebuild = gulp.task('prebuild'),
+    release = gulp.task('release'),
+    js_watcher = gulp.task('js_watcher'),
+    watch = gulp.task('watch');
+
+_default.description = "Prebuild all assets & start watchets.";
+js.description = "Build all Javascript assets.";
+clean_js.description = "Purge all Javascript build assets.";
+clean_all.description = "Clean all build assets.";
+prebuild.description = "Purge & rebuilds required assets.";
+release.description = "Runs all build routines and generates a release.";
+js_watcher.description = "Starts all js file watchers.";
+watch.description = "Start the file watchers.";
